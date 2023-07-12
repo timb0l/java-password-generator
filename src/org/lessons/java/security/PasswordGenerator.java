755fc6f1ce68d2e19bd9975f8faf947a46287a8e
Jam Timbol <jam.timbol00@gmail.com>
2023-07-12 15:22:26 +0200

    Add Main

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,11 @@
+package org.lessons.java.security;
+/*
+-Name,Surname,Color,Birthdate
+-generate password containing the info (print video(?)
+ */
+public class PasswordGenerator {
+
+    public static void main(String[] args) {
+
+    }
+}
